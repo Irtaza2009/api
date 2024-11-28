b25dd1558560c7493eee73f68e69daf1fa8a6830
Muhammad Irtaza <irtazanaqvi05@gmail.com>
2024-11-28 16:58:48 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; 
+
+// route to handle GET requests
+app.get('/', (req, res) => {
+    res.json({
+        city: "Rawalpindi",
+        state: "Punjab",
+        country: "Pakistan",
+        slack_id: "U079EQY9X1D",
+        extra: "Check out my portfolio at https://www.irtaza.xyz/portfolio/ 😁!"
+    });
+});
+
+// starting the server
+app.listen(PORT, () => {
+    console.log(`API is running at http://localhost:${PORT}`);
+});
