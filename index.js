73fe7ecc013677e9a183c4fe147a3c9c091539c1
Muhammad Irtaza <irtazanaqvi05@gmail.com>
2024-11-30 10:20:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get('/', (req, res) => {
     });
 });
 
+// /api route to handle GET requests
+app.get('/api', (req, res) => {
+    res.json({
+        city: "Rawalpindi",
+        state: "Punjab",
+        country: "Pakistan",
+        slack_id: "U079EQY9X1D",
+        extra: "Check out my portfolio at https://www.irtaza.xyz/portfolio/ 😁!"
+    });
+});
+
 // starting the server
 app.listen(PORT, () => {
     console.log(`API is running at http://localhost:${PORT}`);
